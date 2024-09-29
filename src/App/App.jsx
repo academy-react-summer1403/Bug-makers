98b4d9a56c7d379db3a7084f0797a9afeed7ae2a
mehdi asadi <mmasadi2004@gmail.com>
2024-09-29 23:06:59 +0330

    Login step 1

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Header from '../Components/Layout/Header/Header'
+import LoginPage from '../Components/Layout/LoginPages/LoginPage.jsx'
 
 function App() {
 
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+
+      {/* <LoginPage/> */}
     </>
   )
 }
---
+++ src/Components/Layout/LoginPages/LoginPage.jsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+import Login from "./login/Login";
+import ForgetPass from "./passForget/ForgetPass";
+import ReStep1 from "./register/step1/ReStep1";
+import ReStep2 from "./register/step2/ReStep2";
+import ReStep4 from "./register/step4/ReStep4";
+
+const LoginPage = () => {
+    return(
+        <div className="w-full h-lvh flex justify-center items-center bg-white">
+            {/* <Login /> */}
+            {/* <ForgetPass/> */}
+            {/* <ReStep1/> */}
+            <ReStep2/>
+            {/* <ReStep4/> */}
+
+            
+
+        </div>
+    );
+}
+export default LoginPage;
