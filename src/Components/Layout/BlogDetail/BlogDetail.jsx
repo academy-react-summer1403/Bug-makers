0ff1e0f05f97a2f0ef4f98b5981cb397f4dbdbe2
Mohammad sd <mamdbio1386.com>
2024-10-24 12:03:24 +0330

    creat animation page

--- src/Components/Layout/BlogDetail/BlogDetail.jsx
+++ src/Components/Layout/BlogDetail/BlogDetail.jsx
@@ -1,25 +1,29 @@
 import React from "react";
+import { motion } from "framer-motion"; // Import motion
 import BDetailRight from "./BDetailRight/BDetailRight";
 import BDetailLeft from "./BDetailLeft/BDetailLeft";
 import BDetailCenter from "./BDetailCenter/BDetailCenter";
 import { useParams } from "react-router-dom";
 
-const BlogDetail = ()=>{
-    const {id} = useParams()
-    console.log(id)
+const BlogDetail = () => {
+    const { id } = useParams();
+    console.log(id);
 
-
-    return(
-        <div className="m-auto w-full relative text-center bg-[#f5f5f4] ">
-            <div className="w-[85%] mt-[5vw] relative m-auto flex  bg-[#f5f5f4] bg-cover bg-blogBack justify-between">
-                <BDetailRight/>
-                <BDetailCenter key={id} id={id}/>
-                <BDetailLeft/>
-                
-                
+    return (
+        <motion.div 
+            className="m-auto w-full relative text-center bg-[#f5f5f4]"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }} 
+            exit={{ opacity: 0, y: -20 }} 
+            transition={{ duration: 0.5 }} 
+        >
+            <div className="w-[85%] mt-[5vw] relative m-auto flex bg-[#f5f5f4] bg-cover bg-blogBack justify-between">
+                <BDetailRight />
+                <BDetailCenter key={id} id={id} />
+                <BDetailLeft />
             </div>
-
-        </div>
-    )
+        </motion.div>
+    );
 }
-export default BlogDetail
+
+export default BlogDetail;
--- src/Components/Layout/LoginPages/LoginPage.jsx
+++ src/Components/Layout/LoginPages/LoginPage.jsx
@@ -1,22 +1,19 @@
-
 import React from "react";
-import Login from "./login/Login";
-import ForgetPass from "./passForget/ForgetPass";
-import ReStep1 from "./register/step1/ReStep1";
-import ReStep2 from "./register/step2/ReStep2";
-import ReStep4 from "./register/step4/ReStep4";
-import ForgetPassStep2 from "./passForgetStep2/ForgetPassStep2";
+import { motion } from "framer-motion"; // Import motion
 import { Outlet } from "react-router-dom";
 
 const LoginPage = () => {
-    return(
-        <div className="w-full h-lvh flex justify-center items-center bg-white">
-            
-            <Outlet/>
-
-            
-
-        </div>
+    return (
+        <motion.div 
+            className="w-full h-lvh flex justify-center items-center bg-white"
+            initial={{ opacity: 0, y: 20 }} 
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }} 
+            transition={{ duration: 0.5 }} 
+        >
+            <Outlet />
+        </motion.div>
     );
 }
+
 export default LoginPage;
