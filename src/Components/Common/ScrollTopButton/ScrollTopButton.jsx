163d82f5c64e78e55e2bd16ab0530a31980ecad8
mehdi asadi <mmasadi2004@gmail.com>
2024-11-06 23:37:06 +0330

    fix teme for scroll to top

--- src/Components/Common/ScrollTopButton/ScrollTopButton.jsx
+++ src/Components/Common/ScrollTopButton/ScrollTopButton.jsx
@@ -5,7 +5,6 @@ import { FaArrowTurnUp } from "react-icons/fa6";
 
 const ScrollTopButton = () => {
   const [scrollTopBtn, setScrollTopBtn] = useState(false);
-  const { selectedButton } = useSelector((state) => state.themeColor);
 
 //   show button handle scroll
   const handleScroll = () => {
@@ -22,24 +21,25 @@ const ScrollTopButton = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+  const dark = useSelector((state) => state.darkMood);
 
   return (
     <AnimatePresence>
       {scrollTopBtn && (
         <motion.div
           className={`  min-[1700px]:w-16 min-[1700px]:h-16  /* end responsive */ border w-10 z-[1000] h-10 rounded-full fixed bottom-[0.5208333333333334vw] right-[3.15625vw] cursor-pointer flex items-center justify-center
-          ${selectedButton === 0 ? 'bg-blue-600' : ''}
-          ${selectedButton === 1 ? 'bg-green-500' : ''}
-          ${selectedButton === 2 ? 'bg-yellow-500' : ''}`}
-          initial={{ opacity: 0, y: 50 }} 
+          ${dark.selectedButton === 0 ? "bg-blue-500" : ""}
+          ${dark.selectedButton === 1 ? "bg-green-500" : ""}
+          ${dark.selectedButton === 2 ? "bg-yellow-500" : ""}`}
+          initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 50 }} 
-          transition={{ duration: 0.5, ease: 'easeInOut' }} 
+          exit={{ opacity: 0, y: 50 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.9 }}
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} 
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         >
-          <FaArrowTurnUp className='text-white w-[50%] h-[50%]' />
+          <FaArrowTurnUp className="text-white w-[50%] h-[50%]" />
         </motion.div>
       )}
     </AnimatePresence>
