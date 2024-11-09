440169fea1aa9772b3da6ba382aeb4f7ad64addd
mehdi asadi <mmasadi2004@gmail.com>
2024-11-09 10:25:18 +0330

    fix toust

--- src/Core/Services/Api/BlogDetail/addFavorite.js
+++ src/Core/Services/Api/BlogDetail/addFavorite.js
@@ -1,7 +1,12 @@
+import toast from "react-hot-toast";
 import instance from "../../interseptore/Interceptor";
 
-export const AddBlogFavorite = async ({id}) => {
-  const response = await instance.post
-  (`/News/AddFavoriteNews?NewsId=${id}`);
-  return response; 
+export const AddBlogFavorite = async ({ id }) => {
+  try {
+    const response = await instance.post(`/News/AddFavoriteNews?NewsId=${id}`);
+    // toast.success("znvjlaefvkfs");
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 };
--- src/Core/Services/interseptore/Interceptor.js
+++ src/Core/Services/interseptore/Interceptor.js
@@ -21,9 +21,9 @@ const onError = (err) => {
     console.log(err);
     if(err.status === 401){
         removeItem("token")
-        setTimeout(() => {
-            window.location.pathname="/sign/login"            
-        }, 1500);
+        // setTimeout(() => {
+        //     window.location.pathname="/sign/login"            
+        // }, 1500);
         toast.error('ابتدا وارد حساب کاربری خود شوید')
     }
 
