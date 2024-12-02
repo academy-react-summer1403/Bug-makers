d9ca61c024f9149dd6183b20c1efd99f332d8af8
mehdi asadi <mmasadi2004@gmail.com>
2024-12-02 22:12:04 +0330

    fix voice edit

--- src/Components/Common/VoiceAssit/dataTable.jsx
+++ src/Components/Common/VoiceAssit/dataTable.jsx
@@ -1,46 +1,49 @@
-import React from 'react';
-import DataTable from 'react-data-table-component';
+import React from "react";
+import DataTable from "react-data-table-component";
+import { useSelector } from "react-redux";
 
 const DataTableVoice = () => {
   const data = [
-    { id: 1, name: 'خانه', tour: 'home' },
-    { id: 2, name: 'صفحه دوره ها', tour: 'open course' },
-    { id: 3, name: 'صفحه مقالات', tour: 'open blog' },
-    { id: 4, name: 'صفحه پادکست ها', tour: 'open podcast' },
-    { id: 5, name: 'تغییر زبان به انگلیسی', tour: 'change english' },
-    { id: 6, name: 'تغییر زبان به فارسی', tour: 'change persian' },
-    { id: 7, name: 'تغییر زبان به ترکی', tour: 'change turkish' },
-    { id: 8, name: 'تغییر زبان به انگلیسی', tour: 'change english' },
-    { id: 9, name: 'تغییر تم به آبی', tour: 'change to blue' },
-    { id: 10, name: 'تغییر تم به سبز', tour: 'change to green' },
-    { id: 11, name: 'تغییر تم به زرد', tour: 'change to yellow' },
-    { id: 12, name: 'تغییر تم به قرمز', tour: 'change to red' },
-    { id: 13, name: 'تغییر تم به دارک', tour: 'dark mode' },
-    { id: 14, name: 'تغییر تم به لایت', tour: 'light mode' },
-    { id: 14, name: 'ساخت عکس', tour: 'generate image' },
+    { id: 1, name: "خانه", tour: "home" },
+    { id: 2, name: "صفحه دوره ها", tour: "open course" },
+    { id: 3, name: "صفحه مقالات", tour: "open blog" },
+    { id: 4, name: "صفحه پادکست ها", tour: "open podcast" },
+    { id: 5, name: "تغییر زبان به انگلیسی", tour: "change english" },
+    { id: 6, name: "تغییر زبان به فارسی", tour: "change persian" },
+    { id: 7, name: "تغییر زبان به ترکی", tour: "change turkish" },
+    { id: 8, name: "تغییر زبان به انگلیسی", tour: "change english" },
+    { id: 9, name: "تغییر تم به آبی", tour: "change to blue" },
+    { id: 10, name: "تغییر تم به سبز", tour: "change to green" },
+    { id: 11, name: "تغییر تم به زرد", tour: "change to yellow" },
+    { id: 12, name: "تغییر تم به قرمز", tour: "change to red" },
+    { id: 13, name: "تغییر تم به دارک", tour: "dark mode" },
+    { id: 14, name: "تغییر تم به لایت", tour: "light mode" },
+    { id: 15, name: "ساخت عکس", tour: "generate image" },
   ];
 
   // تعریف ستون‌ها
   const columns = [
     {
-      name: 'گفتار',
-      selector: row => row.name,
+      name: "گفتار",
+      selector: (row) => row.name,
     },
     {
-      name: 'دستورات',
-      selector: row => row.tour,
+      name: "دستورات",
+      selector: (row) => row.tour,
     },
   ];
-
+const dark = useSelector((state) => state.darkMood);
   return (
-    <div className='mt-16 '>
+    <div className="mt-2 ">
       <DataTable
         title="تمام دستورات صوتی"
         columns={columns}
         data={data}
         pagination // فعال کردن صفحه‌بندی
+        paginationPerPage={10} // تعداد سطرهای هر صفحه
         striped // اضافه کردن طرح راه راه
         highlightOnHover // روشن کردن حالت برجسته‌سازی هنگام هوور
+        theme={dark.bgHigh == "#ffffff" ? "light" : "dark"}
       />
     </div>
   );
--- src/Components/Common/VoiceAssit/dynamicVoice.jsx
+++ src/Components/Common/VoiceAssit/dynamicVoice.jsx
@@ -4,6 +4,8 @@ import { FiMic } from 'react-icons/fi'; // اضافه کردن آیکون میک
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import DataTableVoice from './dataTable';
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react';
+import { FaMicrophone } from 'react-icons/fa';
 
 const DynamicVoiceWizard = () => {
   const [step, setStep] = useState(1);
@@ -86,183 +88,270 @@ const DynamicVoiceWizard = () => {
     navigate('/')
     toast.success('صداتو ضبط کردم')
   }
-
+const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const dark = useSelector((state) => state.darkMood);
 
   return (
     <div className="h-full">
       <div
         style={{ background: dark.bgHigh, color: dark.textHigh }}
-        className="max-w-4xl mx-auto p-8    rounded-lg shadow-md"
+        className=" w-full h-[17%] p-6 flex justify-between items-center"
       >
-        <h2 className="text-2xl font-bold text-center mb-6">دستیار صوتی</h2>
-
-        <div className="step-content">
-          {step === 1 && (
-            <div>
-              <h3 className="text-xl font-semibold mb-4">مرحله 1: شروع</h3>
-              <p className="mb-4">برای شروع، لطفاً به مرحله بعد بروید.</p>
-              <button
-                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
-                onClick={nextStep}
-              >
-                ادامه دادن
-              </button>
-            </div>
-          )}
-
-          {step === 2 && (
-            <div>
-              <h3 className="text-xl font-semibold mb-4">
-                مرحله 2: انتخاب شورتکات‌ها
-              </h3>
-              <p className="mb-4">لطفاً شورتکات‌های خود را انتخاب کنید.</p>
-              <div className="grid grid-cols-2 gap-4 mb-4">
-                {[
-                  "ClientPanel/dashbord",
-                  "ClientPanel/MyReserve",
-                  "شورتکات 3",
-                  "شورتکات 4",
-                ].map((tag) => (
-                  <div
-                    key={tag}
-                    onClick={() => handleTagSelection(tag)}
-                    className={`cursor-pointer py-2 px-4 rounded text-center border ${
-                      selectedTags.includes(tag)
-                        ? "bg-blue-500 text-white"
-                        : "bg-gray-100 text-gray-700"
-                    }`}
-                  >
-                    {tag}
-                  </div>
-                ))}
-              </div>
-              <button
-                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
-                onClick={nextStep}
-              >
-                مرحله بعد
-              </button>
-              <button
-                className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 mt-2 w-full"
-                onClick={prevStep}
-              >
-                مرحله قبلی
-              </button>
-            </div>
-          )}
-
-          {step === 3 && (
-            <div>
-              <h3 className="text-xl font-semibold mb-4">
-                مرحله 3: تعیین نام دینامیک
-              </h3>
-              <p className="mb-4">
-                لطفاً برای هر تگ، یک نام انحصاری وارد کنید.
-              </p>
-              <div className="grid grid-cols-2 gap-4 mb-4">
-                {selectedTags.map((tag) => (
-                  <div key={tag}>
-                    <label className="block text-gray-700">{tag}</label>
-                    <input
-                      type="text"
-                      className="w-full px-4 py-2 border rounded mt-2"
-                      placeholder="نام انحصاری برای این تگ"
-                      onChange={(e) =>
-                        handleCustomNameChange(tag, e.target.value)
-                      }
-                      value={customNames[tag] || ""}
-                    />
-                  </div>
-                ))}
-              </div>
-              {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-              <button
-                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
-                onClick={nextStep}
-              >
-                مرحله بعد
-              </button>
-              <button
-                className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 mt-2 w-full"
-                onClick={prevStep}
-              >
-                مرحله قبلی
-              </button>
-            </div>
-          )}
-
-          {step === 4 && (
-            <div>
-              <h3 className="text-xl font-semibold mb-4">مرحله 4: ضبط صدا</h3>
-              <p className="mb-4">برای شروع ضبط، روی دکمه میکروفن کلیک کنید.</p>
-              <div className="flex justify-center mb-4">
-                {/* آیکون میکروفن با هاله آبی در هنگام ضبط */}
-                <FiMic
-                  size={50}
-                  className={`cursor-pointer ${
-                    isListening
-                      ? "text-blue-500 animate-pulse"
-                      : "text-gray-700"
-                  }`} // هاله آبی هنگام ضبط
-                />
-              </div>
-              <button
-                className={`px-4 py-2 ${
-                  isListening ? "bg-red-500" : "bg-blue-500"
-                } text-white rounded hover:bg-blue-600 w-full`}
-                onClick={toggleListening}
-                disabled={isRecordingDone}
-              >
-                {isListening ? "در حال ضبط..." : "شروع ضبط"}
-              </button>
-              {isRecordingDone && (
-                <button
-                  className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full mt-4"
-                  onClick={nextStep}
-                >
-                  مرحله بعد
-                </button>
-              )}
-            </div>
-          )}
-          {step === 5 && (
-            <div>
-              <h3 className="text-xl font-semibold mb-4">
-                مرحله 5: نمایش شورتکات‌ها
-              </h3>
-              <p className="mb-4">شورتکات‌ها و نام‌های دینامیک انتخاب شده:</p>
-              {selectedTags.length === 0 ? (
-                <p className="text-sm text-gray-500">
-                  هیچ شورتکاتی انتخاب نشده است.
-                </p>
-              ) : (
-                <ul className="list-disc pl-5">
-                  {selectedTags.map((tag, index) => (
-                    <li key={index} className="text-gray-700">
-                      <strong>{tag}:</strong>{" "}
-                      {customNames[tag] || "نام انحصاری وارد نشده"}
-                    </li>
-                  ))}
-                </ul>
-              )}
-              <button
-                className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 w-full"
-                onClick={lastSubmit}
-              >
-                تأیید و ذخیره
-              </button>
-              <button
-                className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 mt-2 w-full"
-                onClick={prevStep}
-              >
-                مرحله قبلی
-              </button>
-            </div>
-          )}
+        <div className="w-[50%] max-md:w-[70%] h-full flex flex-col justify-between">
+          <span className="block text-[25px] font-bold">دستیار صوتی </span>
+          <div>
+            <span>ویرایش دستورات : </span>
+            <Button onPress={onOpen}>ویرایش</Button>
+          </div>
         </div>
+        <div>
+          <FaMicrophone
+            size={90}
+            color={`${dark.selectedButton === 0 ? "blue" : ""} 
+                  ${dark.selectedButton === 1 ? "green" : ""} 
+                  ${dark.selectedButton === 2 ? "yellow" : ""}
+                  ${dark.selectedButton === 3 ? "red" : ""}`}
+          />
+        </div>
+        <Modal
+          style={{ background: dark.bgHigh, color: dark.textHigh }}
+          backdrop="opaque"
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          size="4xl"
+          motionProps={{
+            variants: {
+              enter: {
+                y: 0,
+                opacity: 1,
+                transition: {
+                  duration: 0.3,
+                  ease: "easeOut",
+                },
+              },
+              exit: {
+                y: -20,
+                opacity: 0,
+                transition: {
+                  duration: 0.2,
+                  ease: "easeIn",
+                },
+              },
+            },
+          }}
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  دستیار صوتی
+                </ModalHeader>
+                <ModalBody>
+                  <div
+                    style={{ background: dark.bgHigh, color: dark.textHigh }}
+                    className="max-w-4xl mx-auto p-8    rounded-lg shadow-md"
+                  >
+                    <h2 className="text-2xl font-bold text-center mb-6">
+                      دستیار صوتی
+                    </h2>
+
+                    <div className="step-content">
+                      {step === 1 && (
+                        <div>
+                          <h3 className="text-xl font-semibold mb-4">
+                            مرحله 1: شروع
+                          </h3>
+                          <p className="mb-4">
+                            برای شروع، لطفاً به مرحله بعد بروید.
+                          </p>
+                          <button
+                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
+                            onClick={nextStep}
+                          >
+                            ادامه دادن
+                          </button>
+                        </div>
+                      )}
+
+                      {step === 2 && (
+                        <div>
+                          <h3 className="text-xl font-semibold mb-4">
+                            مرحله 2: انتخاب شورتکات‌ها
+                          </h3>
+                          <p className="mb-4">
+                            لطفاً شورتکات‌های خود را انتخاب کنید.
+                          </p>
+                          <div className="grid grid-cols-2 gap-4 mb-4">
+                            {[
+                              "ClientPanel/dashbord",
+                              "ClientPanel/MyReserve",
+                              "شورتکات 3",
+                              "شورتکات 4",
+                            ].map((tag) => (
+                              <div
+                                key={tag}
+                                onClick={() => handleTagSelection(tag)}
+                                className={`cursor-pointer py-2 px-4 rounded text-center border ${
+                                  selectedTags.includes(tag)
+                                    ? "bg-blue-500 text-white"
+                                    : "bg-gray-100 text-gray-700"
+                                }`}
+                              >
+                                {tag}
+                              </div>
+                            ))}
+                          </div>
+                          <button
+                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
+                            onClick={nextStep}
+                          >
+                            مرحله بعد
+                          </button>
+                          <button
+                            className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 mt-2 w-full"
+                            onClick={prevStep}
+                          >
+                            مرحله قبلی
+                          </button>
+                        </div>
+                      )}
+
+                      {step === 3 && (
+                        <div>
+                          <h3 className="text-xl font-semibold mb-4">
+                            مرحله 3: تعیین نام دینامیک
+                          </h3>
+                          <p className="mb-4">
+                            لطفاً برای هر تگ، یک نام انحصاری وارد کنید.
+                          </p>
+                          <div className="grid grid-cols-2 gap-4 mb-4">
+                            {selectedTags.map((tag) => (
+                              <div key={tag}>
+                                <label className="block text-gray-700">
+                                  {tag}
+                                </label>
+                                <input
+                                  type="text"
+                                  className="w-full px-4 py-2 border rounded mt-2"
+                                  placeholder="نام انحصاری برای این تگ"
+                                  onChange={(e) =>
+                                    handleCustomNameChange(tag, e.target.value)
+                                  }
+                                  value={customNames[tag] || ""}
+                                />
+                              </div>
+                            ))}
+                          </div>
+                          {errorMessage && (
+                            <p className="text-red-500">{errorMessage}</p>
+                          )}
+                          <button
+                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
+                            onClick={nextStep}
+                          >
+                            مرحله بعد
+                          </button>
+                          <button
+                            className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 mt-2 w-full"
+                            onClick={prevStep}
+                          >
+                            مرحله قبلی
+                          </button>
+                        </div>
+                      )}
+
+                      {step === 4 && (
+                        <div>
+                          <h3 className="text-xl font-semibold mb-4">
+                            مرحله 4: ضبط صدا
+                          </h3>
+                          <p className="mb-4">
+                            برای شروع ضبط، روی دکمه میکروفن کلیک کنید.
+                          </p>
+                          <div className="flex justify-center mb-4">
+                            {/* آیکون میکروفن با هاله آبی در هنگام ضبط */}
+                            <FiMic
+                              size={50}
+                              className={`cursor-pointer ${
+                                isListening
+                                  ? "text-blue-500 animate-pulse"
+                                  : "text-gray-700"
+                              }`} // هاله آبی هنگام ضبط
+                            />
+                          </div>
+                          <button
+                            className={`px-4 py-2 ${
+                              isListening ? "bg-red-500" : "bg-blue-500"
+                            } text-white rounded hover:bg-blue-600 w-full`}
+                            onClick={toggleListening}
+                            disabled={isRecordingDone}
+                          >
+                            {isListening ? "در حال ضبط..." : "شروع ضبط"}
+                          </button>
+                          {isRecordingDone && (
+                            <button
+                              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full mt-4"
+                              onClick={nextStep}
+                            >
+                              مرحله بعد
+                            </button>
+                          )}
+                        </div>
+                      )}
+                      {step === 5 && (
+                        <div>
+                          <h3 className="text-xl font-semibold mb-4">
+                            مرحله 5: نمایش شورتکات‌ها
+                          </h3>
+                          <p className="mb-4">
+                            شورتکات‌ها و نام‌های دینامیک انتخاب شده:
+                          </p>
+                          {selectedTags.length === 0 ? (
+                            <p className="text-sm text-gray-500">
+                              هیچ شورتکاتی انتخاب نشده است.
+                            </p>
+                          ) : (
+                            <ul className="list-disc pl-5">
+                              {selectedTags.map((tag, index) => (
+                                <li key={index} className="text-gray-700">
+                                  <strong>{tag}:</strong>{" "}
+                                  {customNames[tag] || "نام انحصاری وارد نشده"}
+                                </li>
+                              ))}
+                            </ul>
+                          )}
+                          <button
+                            className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 w-full"
+                            onClick={lastSubmit}
+                          >
+                            تأیید و ذخیره
+                          </button>
+                          <button
+                            className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 mt-2 w-full"
+                            onClick={prevStep}
+                          >
+                            مرحله قبلی
+                          </button>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    بستن
+                  </Button>
+                  {/* <Button color="primary" onPress={onClose}>
+                  Action
+                </Button> */}
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
-      <div className='h-[70%] py-4 rounded-lg overflow-auto'>
+
+      <div className="h-[82.5%] max-md:h-[72%] rounded-lg overflow-auto">
         <DataTableVoice />
       </div>
     </div>
